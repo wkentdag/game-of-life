6beec2a16feaebf13d2604a53be0b24540bcfcaf
Shilad Sen <shilad@gmail.com>
2014-02-02 23:45:44 -0600

    Update Conway.java

--- src/edu/macalester/comp124/life/Conway.java
+++ src/edu/macalester/comp124/life/Conway.java
@@ -19,36 +19,7 @@ public class Conway implements RuleSet {
      * @return true if the cell should be alive in the next generation.
      */
     public boolean applyRules(boolean isAlive, int neighborCount) {
-        /*
-        if (isAlive && neighborCount == 2) {
-            return true;
-        } else if (isAlive && neighborCount == 3) {
-            return true;
-        } else if (!isAlive && neighborCount == 3) {
-            return true;
-        }
-        return false;
-        */
-
-        if (isAlive) {
-            return (neighborCount == 2 || neighborCount == 3) ;
-        } else {
-            return (neighborCount == 3);
-        }
-
-        /*
-        if (isAlive && neighborCount < 2) {
-            return false;
-        } else if (isAlive && (neighborCount == 2 || neighborCount == 3)) {
-            return true;
-        } else if (isAlive && neighborCount > 3) {
-            return false;
-        } else if (!isAlive && neighborCount == 3) {
-            return true;
-        } else {
-            return isAlive;
-        }
-        */
+        return true;
     }
 }
 
